324021e55d2edb4bc229a0ebcedfdc8734310671
RamsesGV <senkustone15@gmail.com>
2023-03-07 11:16:40 -0600

    update

--- src/Hoisting/index.js
+++ src/Hoisting/index.js
@@ -11,3 +11,17 @@ function nameOfDog() {
 }
 
 var elmo = 'elmito'
+
+/*
+El hoisting en JavaScript es un comportamiento en el cual las declaraciones de variables y funciones se mueven al principio del ámbito actual. En otras palabras, JavaScript levanta (hoist) las declaraciones de variables y funciones al inicio de su ámbito actual antes de que se ejecuten las instrucciones.
+
+Por lo tanto, se pueden usar variables y funciones antes de declararlas en el código. Cuando una variable se declara con var, su declaración se eleva al inicio del ámbito y se inicializa a undefined. En el caso de las funciones, se eleva su declaración completa al inicio del ámbito, incluyendo su nombre, parámetros y cuerpo.
+
+Es importante tener en cuenta que el hoisting solo eleva las declaraciones, no las asignaciones. Por lo tanto, si se intenta acceder a una variable que se ha declarado, pero no se ha asignado ningún valor, se devolverá undefined.
+
+Es buena práctica declarar todas las variables y funciones al inicio de su ámbito para evitar problemas con el hoisting y para tener un código más legible y fácil de mantener.
+
+
+
+
+*/
